6464f247b1f93cfe2f470320d9476133f93b2316
Danny Alexander Trujillo <daatrujillopu@unal.edu.co>
2016-03-18 14:06:42 -0500

    First Example with Gabriel help

--- 04-backbone/js/router.js
+++ 04-backbone/js/router.js
@@ -1,8 +1,23 @@
 /**
  * Created by danny on 17/03/16.
  */
-define(["vendor/backbone/backbone"], function (Backbone) {
-   var myRoutes = Backbone.Router.extend({
+define(["views/spotifyv1View"], function (spotifyv1) {
+    var myRoutes = Backbone.Router.extend({
+        routes: {
+            "spotifyv1": "onSpotifyurl"
+        },
+        el: null,
+        initialize: function (options) {
+            this.el = options.view;
+            //console.log(this.el);
+        },
 
-   });
+        onSpotifyurl: function () {
+            console.log(this.el);
+            var view = new spotifyv1({container: this.el});
+            console.log("Enter a la ruta");
+        }
+
+    });
+    return myRoutes;
 });
--- 04-backbone/js/views/spotifyv1View.js
+++ 04-backbone/js/views/spotifyv1View.js
@@ -2,10 +2,18 @@
  * Created by DESARROLLO on 18/03/2016.
  */
 define (["text!htmlviews/spotifyv1.html"], function(template){
-    var spotifyv1Viewhtml = Backbone.View.extend({
-        initialize: function () {
-            _.bindAll(this, 'render');
-            this.collection = new
+    var spotifyv1View = Backbone.View.extend({
+        el: null,
+        initialize: function (options) {
+            this.el = options.container;
+            this.render();
+            //console.log(template);
+            /*this.element = element;
+            this.element.innerHTML = template;*/
+        },
+        render: function(){
+            this.el.innerHTML = template;
         }
     });
+    return spotifyv1View;
 });
